496d49929cd65ac44bf9ce24cca1b555ef8eb90b
ZZangZIHO <45262498+ZZangZIHO@users.noreply.github.com>
2020-04-30 13:13:01 +0900

    시작화면 및 UI 구현

---
+++ GolLang/Assets/Scripts/UIEventSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEventSystem : MonoBehaviour
+{
+    public float FadeTime = 1f;
+
+    public GameObject nextCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeIn()
+    {
+        Debug.Log("Fade In");
+    }
+
+    IEnumerator fadeoutplay()
+    {
+
+        yield return null;
+    }
+}
